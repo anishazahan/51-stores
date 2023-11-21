9900a7d1030e866f7744c0cb841213cade3148df
anishazahan <anishazahan13@gmail.com>
2023-11-21 07:51:04 +0600

    heading add

--- src/Components/Layout.js/RootLayout.jsx
+++ src/Components/Layout.js/RootLayout.jsx
@@ -1,9 +1,14 @@
 import Footer from "@/Shared/Footer";
 import Navbar from "@/Shared/Navbar";
+import Head from "next/head";
 
 const RootLayout = ({ children }) => {
   return (
     <div>
+      <Head>
+        <title>Store</title>
+        {/* Add other meta tags, stylesheets, or scripts here */}
+      </Head>
       <Navbar></Navbar>
 
       <div>{children}</div>
