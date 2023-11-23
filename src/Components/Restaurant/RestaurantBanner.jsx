2a31bed3954fcd836b3b72f05e3621129d23366b
anishazahan <anishazahan13@gmail.com>
2023-11-23 15:27:51 +0600

    update

--- src/Components/Restaurant/RestaurantBanner.jsx
+++ src/Components/Restaurant/RestaurantBanner.jsx
@@ -8,7 +8,7 @@ const RestaurantBanner = () => {
         <h2 className="font-bold text-3xl lg:text-6xl max-w-4xl text-white text-center mx-auto">
           Transform Your Restaurant Operations with 51 Store POS System
         </h2>
-        <h2 className="font-bold text-xl lg:text-2xl mx-auto text-white text-center mt-10">
+        <h2 className="font-bold text-xl lg:text-2xl mx-auto text-white text-center mt-10 px-5">
           Intuitive and easy to use point of sale to manage restaurants
         </h2>
       </div>
